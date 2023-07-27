281e51c1511f25d8f66e447914629487e7b04fef
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2023-07-27 15:00:13 +0530

    added game function

--- script.js
+++ script.js
@@ -25,3 +25,35 @@ function getComputerChoice() {
     }
   
   }
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 0; i < 5; i++) {
+      const playerSelection = prompt('Enter your choice: ').toLowerCase();
+      const computerSelection = getComputerChoice();
+  
+      console.log('Round ' + (i + 1));
+      console.log('You chose: ' + playerSelection);
+      console.log('The computer chose: ' + computerSelection);
+  
+      const result = playRound(playerSelection, computerSelection);
+      console.log(result);
+  
+      if (result.includes('win')) {
+        playerScore++;
+      } else if (result.includes('lose')) {
+        computerScore++;
+      }
+    }
+  
+    if (playerScore > computerScore) {
+      console.log('You won the game!');
+    } else if (playerScore < computerScore) {
+      console.log('You lost the game!');
+    } else {
+      console.log('The game was a tie.');
+    }
+  }
+  
+  game(); 
