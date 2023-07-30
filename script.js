eb46794564b48066555e4afe2f321cd93cda6ad3
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2023-07-30 13:06:01 +0530

    Added a basic UI to the JS code

--- script.js
+++ script.js
@@ -1,59 +1,64 @@
 function getComputerChoice() {
-    const choices = (["Rock", "Paper", "Scissors"]).map(el => el.toLowerCase());
-    const randomIndex = (Math.floor(Math.random() * 3));
-    return choices[randomIndex];
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (computerSelection === "scissors" && playerSelection === "rock") ||
+    (computerSelection === "rock" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "scissors")
+  ) {
+    return "You win!";
+  } else if (
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors")
+  ) {
+    return "You lose!";
+  } else {
+    return "It's a tie";
   }
-  
-  function playRound(playerSelection, computerSelection) {
-    if (
-      (computerSelection === 'scissors' && playerSelection === 'rock') ||
-      (computerSelection === 'rock' && playerSelection === 'paper') ||
-      (computerSelection === 'paper' && playerSelection === 'scissors')
-    ) {
-      return 'You win!';
-  
- 
-    } else if (
-      (playerSelection === 'scissors' && computerSelection === 'rock') ||
-      (playerSelection === 'rock' && computerSelection === 'paper') ||
-      (playerSelection === 'paper' && computerSelection === 'scissors')
-    ) {
-      return 'You lose!';
-  
-    } else {
-      return "It's a tie"
-    }
-  
+}
+
+function game(playerSelection) {
+  const computerSelection = getComputerChoice();
+
+  const result = playRound(playerSelection, computerSelection);
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.textContent = `You chose: ${playerSelection}. The computer chose: ${computerSelection}. ${result}`;
+
+  let playerScore = parseInt(document.getElementById("player-score").textContent);
+  let computerScore = parseInt(document.getElementById("computer-score").textContent);
+
+  if (result.includes("win")) {
+    playerScore++;
+  } else if (result.includes("lose")) {
+    computerScore++;
   }
-  function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-  
-    for (let i = 0; i < 5; i++) {
-      const playerSelection = prompt('Enter your choice: ').toLowerCase();
-      const computerSelection = getComputerChoice();
-  
-      console.log('Round ' + (i + 1));
-      console.log('You chose: ' + playerSelection);
-      console.log('The computer chose: ' + computerSelection);
-  
-      const result = playRound(playerSelection, computerSelection);
-      console.log(result);
-  
-      if (result.includes('win')) {
-        playerScore++;
-      } else if (result.includes('lose')) {
-        computerScore++;
-      }
-    }
-  
-    if (playerScore > computerScore) {
-      console.log('You won the game!');
-    } else if (playerScore < computerScore) {
-      console.log('You lost the game!');
-    } else {
-      console.log('The game was a tie.');
-    }
+
+  document.getElementById("player-score").textContent = playerScore;
+  document.getElementById("computer-score").textContent = computerScore;
+
+  if (playerScore === 5) {
+    resultsDiv.textContent += "\n\nYou won the game!";
+    disableButtons();
+  } else if (computerScore === 5) {
+    resultsDiv.textContent += "\n\nYou lost the game!";
+    disableButtons();
   }
-  
-  game(); 
+}
+
+function disableButtons() {
+  const buttons = document.querySelectorAll(".choices button");
+  buttons.forEach(button => button.removeEventListener("click", handleClick));
+}
+
+function handleClick(event) {
+  const playerSelection = event.target.id;
+  game(playerSelection);
+}
+
+const buttons = document.querySelectorAll(".choices button");
+buttons.forEach(button => button.addEventListener("click", handleClick));
